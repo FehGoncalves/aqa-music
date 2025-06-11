98da9959130723e3c686d74e1998e7e22604db06
Christian0307 <christianpedrosa900@gmail.com>
2025-06-10 21:32:34 -0300

    feat (app): implementado botão de acesso

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import { Button } from "./components/Button";
 
 const App = () => {
   return (
-    <div className="flex flex-col h-screen items-center justify-center bg-[#2C2C2C] text-red-700">
+    <div className="flex flex-col h-screen items-center justify-center bg-[#2C2C2C] px-8">
       <figure className="flex items-center">
         <img src='./Logo-2.png' alt="" width={200} height={200} />
       </figure>
 
-     
+      <div className="my-10 w-full flex justify-center">
+        <Button>Entrar</Button>
+      </div>
+
+      <article className="flex flex-col w-full items-center gap-2">
+        <p className="text-[#F1F1F1]">se não tem uma conta, cadastre-se já!</p>
+
+        <Button >Cadastre-se</Button>
+      </article>
+
+
+
     </div>
   );
 }
