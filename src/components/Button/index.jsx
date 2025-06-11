bb82a1e4f2d40bb1a863d299002e9634953772ea
Christian0307 <christianpedrosa900@gmail.com>
2025-06-10 21:32:54 -0300

    feat (app): estilizado botão de acesso

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,6 @@
 export const Button = props => {
     return (
-        <button id={props.id}>{props.children}</button>
+        <button className="bg-[#525B76] text-neutral-50 w-full px-4x py-2 rounded-lg hover:brightness-110 transition-colors text-lg font-semibold max-w-xs" {...props}>{props.children}</button>
+
     )
 }
