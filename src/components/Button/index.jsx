9ec6dbfd2e72fb9336acca8a35be394859ba79bd
Christian0307 <christianpedrosa900@gmail.com>
2025-06-11 21:28:29 -0300

    style (components button): adicionado variantes do botão

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,11 @@
 export const Button = props => {
+    const VARIANTS = {
+        primary: "bg-[#525B76] max-w-xs",
+        secondary: "bg-red-600"
+    }
+
     return (
-        <button className="bg-[#525B76] text-neutral-50 w-full px-4x py-2 rounded-lg hover:brightness-110 transition-colors text-lg font-semibold max-w-xs" {...props}>{props.children}</button>
+        <button className={'w-full px-4x py-2 rounded-lg hover:brightness-110 transition-colors text-lg font-semibold text-neutral-50 ' + VARIANTS[props.variant]} {...props}>{props.children}</button>
 
     )
 }
