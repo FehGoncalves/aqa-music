4f80eec524b323847f5bbd60954e7e1b70e67559
Christian0307 <christianpedrosa900@gmail.com>
2025-06-11 21:37:51 -0300

    feat (pagesLogin): Implementado tela de login

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+export const SignInPage = () => {
+    return (
+        <div className="flex flex-col h-screen items-center justify-center bg-[#2C2C2C] px-8">
+            <figure className="flex items-center">
+                <img src='./Logo-2.png' alt="" width={200} height={200} />
+            </figure>
+
+           
+
+
+        </div>
+    );
+}
