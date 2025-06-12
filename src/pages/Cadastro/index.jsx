44ef5ea96c81c7b61c3ba98f556b24360fe8b851
Christian0307 <christianpedrosa900@gmail.com>
2025-06-11 21:35:15 -0300

    feat (pagesCadastro): Implementado tela de cadastro

---
+++ src/pages/Cadastro/index.jsx
@@ -0,0 +1,31 @@
+import { Button } from "../../components/Button";
+import { InputField } from "../../components/InputField";
+
+export const SignUpPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center bg-[#2C2C2C] p-8">
+
+
+            <section className="bg-slate-600 w-full p-6 rounded-lg flex flex-col items-center justify-center max-w-md">
+                <figure className="flex items-center">
+                    <img src='./Logo-2.png' alt="" width={200} height={200} />
+                </figure>
+
+                <form className="flex flex-col w-full gap-4">
+                    <InputField label='Nome completo' />
+                    <InputField label='Nome fantasia' />
+
+                    <section className="grid lg:grid-cols-2 gap-4">
+                        <InputField label='E-mail' />
+                        <InputField label='Telefone' />
+                    </section>
+
+                    <InputField label='Senha' />
+
+                    <Button variant='secondary'>Finalizar cadastro</Button>
+                </form>
+            </section>
+        </div>
+    );
+}
+
