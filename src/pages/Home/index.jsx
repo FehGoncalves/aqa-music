334cee87675602290307094fcbc09f86b5a8cfb1
Christian0307 <christianpedrosa900@gmail.com>
2025-06-11 21:37:12 -0300

    feat (pagesHome): Implementado tela inicial

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+
+export const HomePage = () => {
+    return (
+        <div className="flex flex-col h-screen items-center justify-center bg-[#2C2C2C] px-8">
+            <figure className="flex items-center">
+                <img src='./Logo-2.png' alt="" width={200} height={200} />
+            </figure>
+
+            <div className="my-10 w-full flex justify-center">
+                <Button><Link to='/login'>Entrar</Link></Button>
+            </div>
+
+            <article className="flex flex-col w-full items-center gap-2">
+                <p className="text-[#F1F1F1]">se não tem uma conta, cadastre-se já!</p>
+
+                <Button ><Link to='/cadastro'>Cadastre-se</Link></Button>
+            </article>
+
+
+
+        </div>
+    );
+}
+
