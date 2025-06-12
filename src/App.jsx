c2b98234e402f84b127e64cd9b7e63031f4d2964
Christian0307 <christianpedrosa900@gmail.com>
2025-06-11 21:29:37 -0300

    feat (app): configurado rotas na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { Button } from "./components/Button";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { HomePage } from "./pages/Home";
+import { SignUpPage } from "./pages/Cadastro";
+import { SignInPage } from "./pages/Login";
 
 
 const App = () => {
   return (
-    <div className="flex flex-col h-screen items-center justify-center bg-[#2C2C2C] px-8">
-      <figure className="flex items-center">
-        <img src='./Logo-2.png' alt="" width={200} height={200} />
-      </figure>
-
-      <div className="my-10 w-full flex justify-center">
-        <Button>Entrar</Button>
-      </div>
-
-      <article className="flex flex-col w-full items-center gap-2">
-        <p className="text-[#F1F1F1]">se não tem uma conta, cadastre-se já!</p>
-
-        <Button >Cadastre-se</Button>
-      </article>
-
-
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cadastro" element={<SignUpPage />} />
+        <Route path="/login" element={<SignInPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
