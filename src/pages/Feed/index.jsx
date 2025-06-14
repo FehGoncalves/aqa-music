7d9a1c50f984675324f06d49ef44793c9041c99d
Christian0307 <christianpedrosa900@gmail.com>
2025-06-13 21:23:03 -0300

    feat (app): implementado FeedPage

---
+++ src/pages/Feed/index.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../components/Navbar"
+
+export const FeedPage = () => {
+    return (
+        <div className="flex bg-[#2C2C2C] px-8>">
+
+            <Navbar />
+        </div>
+    )
+}
